78929d0e3a8c8376edaf0b2a22577ac41dad570d
Arkadiusz Nitka <ark.nitka@gmail.com>
2022-04-06 18:51:31 +0200

    Add bot app, add models for bot

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Search(models.Model):
+    letters = models.CharField(max_length=32)
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_search")
